c26ad26ebb03a8ba9438ab164ed817716642a4af
jsyoon27 <tjddbswkd1@naver.com>
2025-11-18 00:28:29 +0900

    1117

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import { useState } from 'react'
 import { appContainer, board, buttons } from './App.css'
 
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer/reducer";
-import { useDispatch, useSelector } from "react-redux";
 
 const store = configureStore({
     reducer
@@ -9,8 +8,5 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-const dispatch = useDispatch();
-const logger = useSelector((state: RootState) => state.logger);
 
 export default store;
